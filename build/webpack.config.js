9d96106849053cbc6d3e583912bcc7bc4774e3bb
lvdaofeng <574818075@qq.com>
2017-12-19 18:01:22 +0800

    Pp-UI组件库开发环境搭建

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -4,7 +4,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     // 入口文件，path.resolve()方法，可以结合我们给定的两个参数最后生成绝对路径，最终指向的就是我们的index.js文件
-    entry: path.resolve(__dirname, '../app/index/index.js'),
+    entry: path.resolve(__dirname, '../app/index.js'),
     // 输出配置
     output: {
         // 输出路径是 myProject/output/static
@@ -36,8 +36,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            filename: 'app/index/index.html',
-            template: path.resolve(__dirname, '../app/index/index.html'),
+            filename: 'app/index.html',
+            template: path.resolve(__dirname, '../app/index.html'),
             inject: true
         })
     ]
