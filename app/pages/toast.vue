1c9a9f2cd32524d8e891ac45a1c676dd1e272670
lvdaofeng <574818075@qq.com>
2017-12-20 14:50:46 +0800

    修改toast.css

--- app/pages/toast.vue
+++ app/pages/toast.vue
@@ -9,7 +9,6 @@
 <script>
 import Vue from 'Vue'
 import Toast from '../components/Toast/toast.js'
-import '../components/Toast/toast.css'
 Vue.use(Toast);
 export default {
 	name:'toast-test',
@@ -39,51 +38,7 @@ export default {
 </script>
 
 <style>
-.pop-box  {
-    position: fixed;
-    z-index: 1000;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    top: 0;
-    -webkit-tap-highlight-color: rgba(0,0,0,0);
-    -webkit-tap-highlight-color: transparent;
-}
-.pop-box .pop-msg {
-    position: absolute;
-    left: 50%;
-    -webkit-transform: translateX(-50%) translateY(-50%);            
-    transform: translateX(-50%) translateY(-50%);        
-    max-width: 80%;
-    padding: .3rem .8rem;
-    background: rgba(0, 0, 0, .8);
-    color: #fff;
-    border-radius: .3rem;
-    font-size: 14px;
-    word-break: break-all;
-    -webkit-animation: fade-in .5s;
-}
-.pop-box .position-middle {
-    top: 50%;
-}
-.pop-box .position-top {
-    top:5%;
-}
-.pop-box .position-bottom {
-    bottom:5%;
-}
-@-webkit-keyframes fade-in {
-    0% {
-        opacity:0;
-    }
-    50% {
-        opacity:.5;
-    }
-    100% {
-        opacity:1;        
-    }
-}
-
+@import '../components/Toast/toast.css';
 
 .toast-box {
     padding-top: 8rem;
